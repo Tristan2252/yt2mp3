40bff38664a6c459d9e98003342b6509100b6b75
Tristan2252 <tvigil25@live.com>
2016-09-28 10:10:32 -0600

    changed env for alb_add.py

---
+++ alb_add2.py
@@ -0,0 +1,14 @@
+import os.path as path
+import sys
+
+try: 
+    import eyeD3
+except ImportError:
+    import eyed3
+
+
+def main():
+    flags = sys.argv[1:]
+
+if __name__ == "__main__":
+    main()
